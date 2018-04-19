8b98f92da8ab26dd6f53a32f64b55ecac9a020d0
Panchajanya1999 <rsk52959@gmail.com>
2018-04-19 20:24:40 +0530

    My First Applet Program
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ Java/Linerect.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Linerect extends Applet {
+  public void paint(Graphics g) {
+    g.drawOval(100,100,200,100);
+    g.fillOval(180,130,50,50);
+  }
+}

0e9743381bb4390026aefa126bb84843fec2a268
Panchajanya1999 <rsk52959@gmail.com>
2018-05-10 21:21:47 +0530

    Java: Android Logo using Applet
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- Java/Linerect.java
+++ Java/Linerect.java
@@ -3,7 +3,13 @@ import java.awt.*;
 
 public class Linerect extends Applet {
   public void paint(Graphics g) {
-    g.drawOval(100,100,200,100);
-    g.fillOval(180,130,50,50);
+g.setColor(Color.green);
+g.fillArc(500,40,300,400,360,180);//top arch
+g.fillRect(500,270,300,320);//mid body
+g.fillRoundRect(820,270,300,40,10,10);//right hand
+g.fillRoundRect(180,270,300,40,10,10);//left hand
+g.fillRoundRect(500,600,60,235,10,10);//left leg
+g.fillRoundRect(740,600,60,235,10,10);//right leg
+
   }
 }

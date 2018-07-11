8374f4a65680d0e15b075c324715ca4694cd2cd8
Panchajanya1999 <rsk52959@gmail.com>
2018-07-11 20:53:25 +0530

    Prepare 2nd Year Stuffs
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- Java/Hello.java
+++
@@ -1,7 +0,0 @@
-class Hello
-{
-public static void main(String args[])
- {
-  System.out.printf("%s\n","Hello World");
- }
-}
--- Java/Suggestions/CirSq.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-import java.applet.*;
-public class CirSq extends Applet {
-  public void paint(Graphics g){
-    g.drawOval(110,110,50,50);
-    g.drawRect(115,120,40,30);
-  }
-}

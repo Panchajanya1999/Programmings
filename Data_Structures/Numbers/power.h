b43ab8a43e4fe0bc88258e4de8efa09e9d742f64
Panchajanya1999 <rsk52959@gmail.com>
2018-11-07 11:22:20 +0530

    DS: Power Calculation dependency included
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ Data_Structures/Numbers/power.h
@@ -0,0 +1,28 @@
+/*
+ * Power using Recursion in C
+ *
+ * Copyright (c) 2018 Panchajanya1999<rsk52959@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+long power(int x, unsigned int y) 
+{ 
+    if (y == 0) 
+        return 1; 
+    else if (y%2 == 0) 
+        return power(x, y/2)*power(x, y/2); 
+    else
+        return x*power(x, y/2)*power(x, y/2); 
+} 

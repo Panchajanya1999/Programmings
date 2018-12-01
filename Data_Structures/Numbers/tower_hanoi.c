b927abc0aaca71996109123a36dd1be687f1859a
Panchajanya1999 <rsk52959@gmail.com>
2018-12-01 22:01:12 +0530

    Data Structures: Tower Of Hanoi Tower of Hanoi using Recursion in C [dependency included]
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ Data_Structures/Numbers/tower_hanoi.c
@@ -0,0 +1,28 @@
+/*
+ * Tower of Hanoi using Recursion in C
+ *
+ * Copyright (c) 2018 Panchajanya1999 <rsk52959@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+ 
+#include <stdio.h> 
+#include "tower_hanoi.h"
+  
+int main() 
+{ 
+    int n = 4; // Number of disks 
+    towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods 
+    return 0; 
+} 
---
+++ Data_Structures/Numbers/tower_hanoi.h
@@ -0,0 +1,31 @@
+/*
+ * Tower of Hanoi dependency using Recursion in C
+ *
+ * Copyright (c) 2018 Panchajanya1999 <rsk52959@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+// C recursive function to solve tower of hanoi puzzle 
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
+{ 
+    if (n == 1) 
+    { 
+        printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod); 
+        return; 
+    } 
+    towerOfHanoi(n-1, from_rod, aux_rod, to_rod); 
+    printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod); 
+    towerOfHanoi(n-1, aux_rod, to_rod, from_rod); 
+} 

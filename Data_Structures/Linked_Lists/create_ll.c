0c9dbe080525c2d1ecd6003c30e81f44433c4f2d
Panchajanya1999 <rsk52959@gmail.com>
2018-12-05 00:50:49 +0530

    DS: Modify Linked List Creation using C
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- Data_Structures/Linked_Lists/create_ll.c
+++ Data_Structures/Linked_Lists/create_ll.c
@@ -1,5 +1,25 @@
+/*
+ * Create Linked List in C
+ *
+ * Copyright (c) 2018 Panchajanya1999<rsk52959@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
  struct node {
  int data;
  struct node *link;
@@ -10,18 +30,20 @@ int main() {
  void display(struct node *start);
  struct node *start;
  start=create();
+ printf("\nThe created Linked List is: \t\a");
  display(start);
  }
 
 struct node *create(void) {
  struct node *r,*p,*n;
- int s,k;
- char x;
+ int s,k,count=0;
+ int x;
  r=NULL;
  s=sizeof(struct node);
- do {
+ while(1) {
      printf("\nEnter an Element : ");
      scanf("%d",&k);
+     count++;
      n=(struct node *)malloc(s);
      n->data=k;
      n->link=NULL;
@@ -30,11 +52,16 @@ struct node *create(void) {
      
      p=n;
      printf("\nAnymore element(s) ?");
-     printf(" Y/N\n");
+     printf("\nPress 1 or 0\n");
+     scanf("%d",&x);
+     if(x==0) {
+     	     printf("\n%d Node(s) Created\n\n",count);
+     	     break;
+      } 	     
      fflush(stdin);
-     x=getchar();
+     //x=getchar();
  
- }while(strncmp(x,"Y")=0); 
+ }//while(strncmp(x,"Y",1)==0); 
  return (r);
  }    
 
@@ -46,7 +73,7 @@ void display(struct node *start) {
    p=p->link;
  }
  
-printf("END");
+printf("END\n\n");
 return;
 }        
  

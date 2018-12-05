39fe1d7f764cfc0def8119c7409da12fbafb27dd
Panchajanya1999 <rsk52959@gmail.com>
2018-12-05 23:57:59 +0530

    OS: Best Fit in C
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ OS/Best_Fit.c
@@ -0,0 +1,71 @@
+/*
+ * Best Fit Allocation in C
+ *
+ * Copyright (c) 2018 Panchajanya1999<rsk52959@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include<stdio.h>
+ 
+void main()
+{
+    int fragment[20],b[20],p[20],i,j,nb,np,temp,lowest=9999;
+    static int barray[20],parray[20];
+    
+    printf("\n\t\t\tMemory Management Scheme - Best Fit\n\n");
+    printf("\nEnter the number of blocks:");
+    scanf("%d",&nb);
+    printf("Enter the number of processes:");
+    scanf("%d",&np);
+    
+    printf("\nEnter the size of the blocks:-\n");
+    for(i=1;i<=nb;i++)
+    {
+        printf("Block no.%d:",i);
+        scanf("%d",&b[i]);
+    }
+    
+    printf("\nEnter the size of the processes :-\n");
+    for(i=1;i<=np;i++)
+    {
+        printf("Process no.%d:",i);
+        scanf("%d",&p[i]);
+    }
+    
+    for(i=1;i<=np;i++)
+    {
+        for(j=1;j<=nb;j++)
+        {
+            if(barray[j]!=1)
+            {
+                temp=b[j]-p[i];
+                if(temp>=0)
+                    if(lowest>temp)
+                    {
+                        parray[i]=j;
+                        lowest=temp;
+                    }
+            }
+        }
+        
+        fragment[i]=lowest;
+        barray[parray[i]]=1;
+        lowest=10000;
+    }
+    
+    printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment");
+    for(i=1;i<=np && parray[i]!=0;i++)
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,p[i],parray[i],b[parray[i]],fragment[i]);
+}

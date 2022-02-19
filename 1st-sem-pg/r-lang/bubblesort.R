191dbd5ef4dc36293b4c2a11bf997e56f1ff2f37
Panchajanya1999 <rsk52959@gmail.com>
2022-02-19 21:50:53 +0530

    1st-sem-pg/r-lang: Add Bubble Sort
    Change-Id: I6bc44d2be85a5fdda52c5eb747ddbe8068e8b418
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ 1st-sem-pg/r-lang/bubblesort.R
@@ -0,0 +1,23 @@
+#! /usr/bin/ Rscript
+
+set <- c(10, 9, 8,7,6,5,4,3,2,1)
+
+bubblesort <- function(x) {
+  n <- length(x)
+  for(i in 1:(n-1)) {
+    for(j in 1:(n-i)) {
+      if (x[j] > x[j+1]){
+        temp <- x[j]
+        x[j] <- x[j+1]
+        x[j+1] <- temp
+      }
+    }
+  }
+  return (x)
+}
+
+out <- bubblesort(set)
+
+cat("The original dataset was -", set)
+cat("\nThe sorted dataset is - ", out)
+

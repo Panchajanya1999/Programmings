8d1c1d6e4a56424d8b6e28e00b84e0806fb9b709
Panchajanya1999 <rsk52959@gmail.com>
2022-11-24 11:41:24 +0530

    linkedlist: update with more basic functionalities
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- python/llist.py
+++ python/llist.py
@@ -20,6 +20,48 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    # prints the linked list
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data)
+            curr_node = curr_node.next
+
+    # append will insert an element at the end of the linked list
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    # prepend will insert an element at the beginning of the linked list
+    def prepend():
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+    # insert a node after a given node
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node is non-existent")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        self.head = new_node
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+llist.insert_after_node(llist.head.next,"E")
+
+llist.print_list()
+
 
 
 

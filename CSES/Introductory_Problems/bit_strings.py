f747f0d7d4d24cb0afc7cddf3e410c2d34aad6a0
Panchajanya1999 <rsk52959@gmail.com>
2023-09-28 11:58:00 +0530

    CSES: prepare to shift to a new branch
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- CSES/Introductory_Problems/bit_strings.py
+++
@@ -1,9 +0,0 @@
-# Bit Strings
-# CSES Link : https://cses.fi/problemset/task/1617
-
-def bit_strings(n):
-    return 2**n % (10**9 + 7)
-
-if __name__ == "__main__":
-    n = int(input())
-    print(bit_strings(n))
